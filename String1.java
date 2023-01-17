f2cc80d9255650c551dcda1868da0786cb1efe89
mallelavinaykumar1999 <118715884+mallelavinaykumar1999@users.noreply.github.com>
2023-01-17 16:20:42 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,10 @@
+/* write a program to read string from user and print that string*/
+import java.util.*;
+public class String1{
+  public static void main(String args []){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your string :");
+    String st=sc.next();
+    System.out.println(" your string is :"+ st);
+  }
+}
